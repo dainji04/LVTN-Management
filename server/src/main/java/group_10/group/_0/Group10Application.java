22eb34e9e82e3bfce5574ef7ee5486deb7c40d58
Nguyễn Hồng Minh Quân <nguyenhongquan20042005@gmail.com>
2026-02-27 21:36:17 +0700

    Init Spring boot project

---
+++ server/src/main/java/group_10/group/_0/Group10Application.java
@@ -0,0 +1,13 @@
+package group_10.group._0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group10Application.class, args);
+	}
+
+}
