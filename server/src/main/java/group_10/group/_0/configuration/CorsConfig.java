e3da32c8dee3ea54e0a428d3ab4ed1f8eeb991f1
Nguyen Hong Minh Quan <nguyenhongquan20042005@gmail.com>
2026-03-28 23:24:35 +0700

    Update CORS configuration to allow all domains and simplify origin handling

--- server/src/main/java/group_10/group/_0/configuration/CorsConfig.java
+++ server/src/main/java/group_10/group/_0/configuration/CorsConfig.java
@@ -11,18 +11,32 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        config.setAllowedOrigins(List.of(
+//                "https://social.dainji.id.vn",
+//                "https://test-social.dainji.id.vn",
+//                "http://localhost:3000",
+//                "http://localhost:8080",
+//                "http://localhost:5173"
+//        ));
+//
+//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        config.setAllowedHeaders(List.of("*"));
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsFilter(source);
+//    }
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-                "https://social.dainji.id.vn",
-                "https://test-social.dainji.id.vn",
-                "http://localhost:3000",
-                "http://localhost:8080",
-                "http://localhost:5173"
-        ));
-
+        config.addAllowedOriginPattern("*"); // cho phép tất cả domain
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
