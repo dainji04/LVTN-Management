22e21044f7d6ecb16c58645d1214bdc9e70fadb3
Nguyễn Hồng Minh Quân <nguyenhongquan20042005@gmail.com>
2026-04-06 20:59:41 +0700

    Update Dockerfile and GatewayConfig to fix 502 error and clean proxy headers

--- server/src/main/java/group_10/group/_0/GatewayConfig.java
+++ server/src/main/java/group_10/group/_0/GatewayConfig.java
@@ -10,15 +10,15 @@ public class GatewayConfig {
     @Bean
     public RestClientCustomizer cleanProxyHeadersCustomizer() {
         return builder -> builder.requestInterceptor((request, body, execution) -> {
-            // 1. Tiêu diệt toàn bộ các header proxy do Gateway tự đẻ ra
+            // Dọn sạch rác Proxy
             request.getHeaders().remove("X-Forwarded-Host");
             request.getHeaders().remove("X-Forwarded-Port");
             request.getHeaders().remove("X-Forwarded-Proto");
             request.getHeaders().remove("X-Forwarded-For");
             request.getHeaders().remove("Forwarded");
 
-            // 2. Ép cứng đích đến là host của Chat Service Node.js
-            request.getHeaders().set("Host", "chat-service-social-media.onrender.com");
+            // Ép cứng Host thành domain thật của Node.js (KHÔNG CÓ https://)
+            request.getHeaders().set("Host", "lvtn-management-14vw.onrender.com");
 
             return execution.execute(request, body);
         });
